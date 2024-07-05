f360c74362a352d3d1020e45d7fcb1aa00ebdd4f
LevanNamicheishvili <117360031+LevanNamicheishvili@users.noreply.github.com>
2024-07-06 01:40:11 +0400

    core

---
+++ Methods.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Methods
+{
+    internal class BinarySearch
+    {
+        private int[] array;
+        private int target;
+
+        public void SearchParams(int[] array, int target)
+        {
+            if (array == null || array.Length == 0)
+            {
+                throw new ArgumentException(nameof(array));
+            }
+
+            this.array = array;
+            this.target = target;
+        }
+
+        public int PerformBinarySearch()
+        {
+            if (array == null || array.Length == 0)
+            {
+                throw new InvalidOperationException();
+
+            }
+
+            int left = 0;
+            int right = array.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+
+                if (array[mid] == target)
+                {
+                    return mid;
+                }
+
+                if (array[mid] < target)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("enter arr elemetns : ");
+
+            string input = Console.ReadLine();
+            int[] array = input.Split(' ')
+                               .Select(int.Parse)
+                               .ToArray();
+
+            Array.Sort(array);
+
+            Console.WriteLine("enter target : ");
+            int target = int.Parse(Console.ReadLine());
+
+            BinarySearch search = new BinarySearch();
+
+            search.SearchParams(array, target);
+
+            int result = search.PerformBinarySearch();
+            if (result == -1)
+            {
+                Console.WriteLine("element not found");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("element index " + result);
+                Console.ReadKey();
+            }
+        }
+    }
+}
